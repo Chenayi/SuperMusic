e92d9378bae9ad2407ea3ddccae892bf8e37f004
Chenayi <309107361@qq.com>
2018-04-10 21:37:10 +0800

    优化dagger2使用bug

--- app/src/main/java/com/chenayi/supermusic/base/BasePresenter.kt
+++ app/src/main/java/com/chenayi/supermusic/base/BasePresenter.kt
@@ -1,18 +1,15 @@
 package com.chenayi.supermusic.base
 
 import android.content.Context
-import javax.inject.Inject
 
 /**
  * Created by Chenwy on 2018/4/10.
  */
 open class BasePresenter<V : IView>(context: Context?, view: V?) : IPresenter {
-    var mContext: Context? = null;
-    var mView: V? = null;
+    var mContext: Context? = context;
+    var mView: V? = view;
 
     init {
-        this.mContext = context;
-        this.mView = view;
         this.onStart();
     }
 }
